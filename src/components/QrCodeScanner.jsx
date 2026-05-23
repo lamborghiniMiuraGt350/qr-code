0c323a9a2a4bb5d5881245e2d48f191dc4d10406
lamborghiniMiuraGt350 <uc4iha.sora@gmail.com>
2026-05-24 02:56:58 +0300

    last

--- src/components/QrCodeScanner.jsx
+++ src/components/QrCodeScanner.jsx
@@ -1,42 +1,26 @@
-import { QrReader } from 'react-qr-reader';
-import { useState } from 'react';
+import { Scanner } from '@yudiel/react-qr-scanner'
+import { useState } from 'react'
 import s from './QrCodeScanner.module.css'
 import { SCAN_DATA } from '../constans'
+
 export function QrCodeScanner() {
     const [scanned, setScanned] = useState(null)
+
     const scanHandle = (result) => {
-        if (!result) return;
-
-        const prevData = JSON.parse(localStorage.getItem(SCAN_DATA) || '[]');
-
-        if (prevData.includes(result.text)) return;
-
-        setScanned(result.text);
-
-        localStorage.setItem(
-            SCAN_DATA,
-            JSON.stringify([...prevData, result.text])
-        );
+        if (!result) return
+        const prev = JSON.parse(localStorage.getItem(SCAN_DATA) || '[]')
+        if (prev.includes(result[0].rawValue)) return
+        setScanned(result)
+        localStorage.setItem(SCAN_DATA, JSON.stringify([...prev, result[0].rawValue]))
     }
 
-    const settings = {
-        audio: false,
-        // finder:false
-    }
     return (
         <div className={s.container}>
-            <QrReader
-                constraints={{ facingMode: 'environment' }}
-                scanDelay={1000}
-                onResult={scanHandle}
-                containerStyle={{ width: '500px' }}
-            />
-            {/* <Scanner
-                components={settings}
-                onScan={scanHandle}
-                styles={{ container: { width: 350 } }}
+            <Scanner
+                onScan={(result) => scanHandle(result[0].rawValue)}
                 onError={(error) => console.log(error?.message)}
-            /> */}
+            />
             <p className={s.result}>{scanned}</p>
-        </div>)
+        </div>
+    )
 }
